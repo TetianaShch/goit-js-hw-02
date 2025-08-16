9e88bd1e673a760dfdc8cb52f80be9c0a8e4267d
Tetiana Shcherbyna <tshcherbinka@icloud.com>
2025-08-16 19:05:37 +0300

    fix: correct mistakes in formatMessage function

--- js/task-2.js
+++ js/task-2.js
@@ -1,14 +1,6 @@
-// function formatMessage(message, maxLength) {
-//   if (message.length >= maxLength) {
-//        return message;
-//    }
-//    return message.slice(0, maxLength) + "...";
-//}
-
 function formatMessage(message, maxLength) {
-    return message.length > maxLength ? message.slice(0, maxLength) + "..." : message;
+    return message.length <= maxLength ? message : message.slice(0, maxLength) + "...";
 }
-
 
 
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
